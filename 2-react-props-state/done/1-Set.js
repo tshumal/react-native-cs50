ccbd7fa04e498b124822d67eec0acf7bceac0f75
Lingani Tshuma <tshumal@gmail.com>
2021-08-09 11:57:23 +0200

    2-react-props-state

--- 2-react-props-state/done/1-Set.js
+++ 2-react-props-state/done/1-Set.js
@@ -2,25 +2,25 @@
 // It should also have the ability to get its size
 
 class Set {
-    constructor(arr) {
-        this.arr = arr;
-    }
+  constructor(arr) {
+    this.arr = arr
+  }
 
-    add(val) {
-        if(!this.has(val)) this.arr.push(val)
-    }
+  add(val) {
+    if (!this.has(val)) this.arr.push(val)
+  }
 
-    delete(val) {
-        this.arr = this.arr.filter(x => x !== val)
-    }
+  delete(val) {
+    this.arr = this.arr.filter(x => x !== val)
+  }
 
-    has(val) {
-        return this.arr.includes(val)
-    }
+  has(val) {
+    return this.arr.includes(val)
+  }
 
-    get size() {
-        return this.arr.length
-    }
+  get size() {
+    return this.arr.length
+  }
 }
 
 const s = new Set([1,2,3,4,5])
